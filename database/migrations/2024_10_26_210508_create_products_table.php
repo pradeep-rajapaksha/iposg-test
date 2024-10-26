3254fbdfef6820a3ed6e2acebba08a8e846b0758
pradeep-rajapaksha <pradeepprasanna.rajapaksha4@gmail.com>
2024-10-27 03:16:21 +0530

    first commit

---
+++ database/migrations/2024_10_26_210508_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->decimal('price', total: 8, places: 2);
+            $table->integer('stock_qty');
+            $table->boolean('status')->default(true);
+            $table->timestamps();
+            $table->softDeletes();
+            
+            $table->index(['name']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
---
+++ database/migrations/2024_10_26_210555_create_product_categories_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_categories', function (Blueprint $table) {
+            $table->foreignId('product_id');
+            $table->foreignId('category_id');
+            $table->timestamps();
+            
+            $table->primary(['product_id', 'category_id']);
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_categories');
+    }
+};
